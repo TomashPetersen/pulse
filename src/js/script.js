d381a67fd6ce9b0c11db604256d98f5103003dd7
Denis Volkov <d.g.volkovbz@gmail.com>
2021-07-15 18:47:57 +0400

    Added carousel section with tiny-slider v1.0, no full adaptivity, slick-slider was removed

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+/* $(document).ready(function () {
   $(".carousel__inner").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -18,4 +18,21 @@ $(document).ready(function () {
       },
     ],
   });
+}); */
+
+const slider = tns({
+  container: ".carousel__inner",
+  items: 1,
+  slideBy: "page",
+  autoplay: false,
+  controls: false,
+  nav: false,
+});
+
+document.querySelector(".prev").addEventListener("click", function () {
+  slider.goTo("prev");
+});
+
+document.querySelector(".next").addEventListener("click", function () {
+  slider.goTo("next");
 });
