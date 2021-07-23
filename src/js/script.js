3df05c411d49d32c3eb3467d19f1ee3a693b200b
Denis Volkov <d.g.volkovbz@gmail.com>
2021-07-23 17:03:43 +0400

    feat: create nav-dots functionality of the carousel block from 768px to 991px v^1.0

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,23 @@
 /* $(document).ready(function () {
-  $(".carousel__inner").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    speed: 1200,
-    adaptiveHeight: true,
-    prevArrow:
-      '<button type="button" class="slick-prev"><img src="img/icons/left-arrow.svg"></button>',
-    nextArrow:
-      '<button type="button" class="slick-next"><img src="img/icons/right-arrow.svg"></button>',
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          dots: true,
-          arrows: false,
-        },
-      },
-    ],
-  });
+	$(".carousel__inner").slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		speed: 1200,
+		adaptiveHeight: true,
+		prevArrow:
+			'<button type="button" class="slick-prev"><img src="img/icons/left-arrow.svg"></button>',
+		nextArrow:
+			'<button type="button" class="slick-next"><img src="img/icons/right-arrow.svg"></button>',
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					dots: true,
+					arrows: false,
+				},
+			},
+		],
+	});
 }); */
 
 const slider = tns({
@@ -43,3 +43,63 @@ document.querySelector(".prev").addEventListener("click", function () {
 document.querySelector(".next").addEventListener("click", function () {
   slider.goTo("next");
 });
+
+document
+  .querySelector(".nav-dots__item_0")
+  .addEventListener("click", function (e) {
+    slider.goTo("0");
+    if (
+      !document
+        .querySelector(".nav-dots__item_0")
+        .classList.contains("nav-dots__item_active")
+    ) {
+      for (let sibling of document.querySelector(".nav-dots__item_0").parentNode
+        .children) {
+        sibling.classList.remove("nav-dots__item_active");
+      }
+
+      document
+        .querySelector(".nav-dots__item_0")
+        .classList.toggle("nav-dots__item_active");
+    }
+  });
+
+document
+  .querySelector(".nav-dots__item_1")
+  .addEventListener("click", function (e) {
+    slider.goTo("1");
+    if (
+      !document
+        .querySelector(".nav-dots__item_1")
+        .classList.contains("nav-dots__item_active")
+    ) {
+      for (let sibling of document.querySelector(".nav-dots__item_1").parentNode
+        .children) {
+        sibling.classList.remove("nav-dots__item_active");
+      }
+
+      document
+        .querySelector(".nav-dots__item_1")
+        .classList.toggle("nav-dots__item_active");
+    }
+  });
+
+document
+  .querySelector(".nav-dots__item_2")
+  .addEventListener("click", function (e) {
+    slider.goTo("2");
+    if (
+      !document
+        .querySelector(".nav-dots__item_2")
+        .classList.contains("nav-dots__item_active")
+    ) {
+      for (let sibling of document.querySelector(".nav-dots__item_2").parentNode
+        .children) {
+        sibling.classList.remove("nav-dots__item_active");
+      }
+
+      document
+        .querySelector(".nav-dots__item_2")
+        .classList.toggle("nav-dots__item_active");
+    }
+  });
